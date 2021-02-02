7c186627822b037af4192425ca8ccf753d949f46
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-02-02 11:08:18 -0300

    Welcome greeting

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 menu = Menu()
-machine = MoneyMachine()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+is_on = True
 
+print('''\033[33m
+         )))
+        (((
+      +-----+
+      |     |] - WELCOME TO THE COFFEE MACHINE!
+      `-----' 
 
+      ------ MENU ------ 
+      Espresso ($1.50)
+      Latte ($2.50)
+      Cappuccino ($3.00)
+      ------------------
+
+      PS: Type "report" at any moment
+      to check our resources available.
+      Type "off" to log out from the machine.\033[m
+    ''')
