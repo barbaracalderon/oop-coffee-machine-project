0c51c0d97d4a9cee7dfeab838bfc7b71818fd60a
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-02-02 10:52:34 -0300

    Work in progress

--- main.py
+++ main.py
@@ -6,10 +6,3 @@ menu = Menu()
 machine = MoneyMachine()
 
 
-user_choice = str(input(f'Welcome to the Coffee Machine!\nWhat would you like? ({menu.get_items()}): '))
-if user_choice == 'report':
-    machine.report()
-elif menu.find_drink(user_choice) is not None:
-    menu_item = MenuItem(user_choice)
-    machine.process_coins()
-    machine.make_payment(menu_item.cost)
