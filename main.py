bb23d96ee2075243e8787d7499773b24dfdc61a6
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-02-02 11:52:51 -0300

    Errors treatment

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-menu = Menu()
-money_machine = MoneyMachine()
-coffee_maker = CoffeeMaker()
-is_on = True
-
 print('''\033[33m
          )))
         (((
@@ -24,3 +19,21 @@ print('''\033[33m
       to check our resources available.
       Type "off" to log out from the machine.\033[m
     ''')
+
+
+menu = Menu()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    user_choice = str(input(f'What would you like?\nOptions ({options}): ')).strip().lower()
+    if user_choice == 'off':
+        print('\033[31m<<THE END>>\033[m')
+        is_on = False
+    elif user_choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    elif menu.find_drink(user_choice) is None:
+        print('\033[31mError. Please choose an available option.\033[m')
