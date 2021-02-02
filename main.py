6ab1c5cd21d67e6b38f86e7d8bb0effcd99dffa3
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-02-02 12:29:33 -0300

    Sufficient resources and payment

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-print('''\033[33m
-         )))
-        (((
-      +-----+
-      |     |] - WELCOME TO THE COFFEE MACHINE!
-      `-----' 
-
-      ------ MENU ------ 
-      Espresso ($1.50)
-      Latte ($2.50)
-      Cappuccino ($3.00)
-      ------------------
-
-      PS: Type "report" at any moment
-      to check our resources available.
-      Type "off" to log out from the machine.\033[m
-    ''')
+def welcome():
+    print('''\033[33m
+             )))
+            (((
+          +-----+
+          |     |] - WELCOME TO THE COFFEE MACHINE!
+          `-----' 
+    
+          ------ MENU ------ 
+          Espresso ($1.50)
+          Latte ($2.50)
+          Cappuccino ($3.00)
+          ------------------
+    
+          PS: Type "report" at any moment
+          to check our resources available.
+          Type "off" to log out from the machine.\033[m
+        ''')
 
 
 menu = Menu()
@@ -27,6 +28,7 @@ coffee_maker = CoffeeMaker()
 is_on = True
 
 while is_on:
+    welcome()
     options = menu.get_items()
     user_choice = str(input(f'What would you like?\nOptions ({options}): ')).strip().lower()
     if user_choice == 'off':
@@ -37,3 +39,10 @@ while is_on:
         money_machine.report()
     elif menu.find_drink(user_choice) is None:
         print('\033[31mError. Please choose an available option.\033[m')
+    else:
+        beverage = menu.find_drink(user_choice) #Encapsulates the result
+        sufficient_resources = coffee_maker.is_resource_sufficient(beverage) #TrueFalse result
+        sufficient_money = money_machine.make_payment(beverage.cost)
+        if sufficient_resources and sufficient_money:
+            print('Done! Allow us to make your beverage now.')
+            coffee_maker.make_coffee(beverage)
